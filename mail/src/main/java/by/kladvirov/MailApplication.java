957b1936c00561f93f55af92157f4a4e0f44ddce
Vlad Kirov <wladponomar@gmail.com>
2024-04-16 20:53:52 +0300

    feat: main app

--- mail/src/main/java/by/kladvirov/MailApplication.java
+++ mail/src/main/java/by/kladvirov/MailApplication.java
@@ -1,19 +1,13 @@
 package by.kladvirov;
 
-import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MailApplication {
 
-    @SneakyThrows
     public static void main(String[] args) {
-
-        ApplicationContext context = SpringApplication.run(MailApplication.class, args);
-        Example example = context.getBean(Example.class);
-        example.send();
+        SpringApplication.run(MailApplication.class);
     }
 
 }
