d35372ba5829cdfd0f5c56188e977ea3726628ea
Vlad Kirov <wladponomar@gmail.com>
2024-04-26 18:58:35 +0300

    feat: custom exceptions

---
+++ auth/src/main/java/by/kladvirov/exception/PasswordException.java
@@ -0,0 +1,9 @@
+package by.kladvirov.exception;
+
+public class PasswordException extends RuntimeException {
+
+    public PasswordException(String message) {
+        super(message);
+    }
+
+}
---
+++ auth/src/main/java/by/kladvirov/exception/TokenException.java
@@ -0,0 +1,9 @@
+package by.kladvirov.exception;
+
+public class TokenException extends RuntimeException {
+
+    public TokenException(String message) {
+        super(message);
+    }
+
+}
