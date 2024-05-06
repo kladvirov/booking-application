8ec5c6e39abbab89cb7f5b068e3e25ca744e12ce
Androoon <andrejsuslov308@gmail.com>
2024-05-06 09:05:18 +0300

    feat: added methods in interface

--- core/src/main/java/by/kladvirov/service/ReservationService.java
+++ core/src/main/java/by/kladvirov/service/ReservationService.java
@@ -2,7 +2,6 @@ package by.kladvirov.service;
 
 import by.kladvirov.dto.ReservationCreationDto;
 import by.kladvirov.dto.ReservationDto;
-import by.kladvirov.model.Reservation;
 
 import java.util.List;
 
@@ -12,10 +11,22 @@ public interface ReservationService {
 
     List<ReservationDto> findAll();
 
+    List<ReservationDto>findAllByUsername(String username);
+
     ReservationDto save(ReservationCreationDto dto);
 
     void update(Long id, ReservationCreationDto dto);
 
     void delete(Long id);
+
+    boolean isAllowedToRead(String username, Long id);
+
+    boolean isAllowedToCreate(String firstUsername, String secondUsername);
+
+    boolean isAllowedToUpdate(String firstUsername, String secondUsername);
+
+    boolean isAllowedToDelete(String username, Long id);
+
+    boolean isAdmin(String header);
 
 }
