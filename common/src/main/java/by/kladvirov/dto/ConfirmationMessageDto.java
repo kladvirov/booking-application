236796fceffd4454cd565866f70f83855fd712fa
Vlad Kirov <wladponomar@gmail.com>
2024-06-14 00:18:10 +0300

    refactor: optimized imports

--- common/src/main/java/by/kladvirov/dto/ConfirmationMessageDto.java
+++ common/src/main/java/by/kladvirov/dto/ConfirmationMessageDto.java
@@ -4,13 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
 
 @Data
 @Builder
 @AllArgsConstructor
 @RequiredArgsConstructor
-public class ConfirmationMessageDto implements Message{
+public class ConfirmationMessageDto implements Message {
 
     private String name;
 
