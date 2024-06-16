20f5b5970e4841cc1ca694f7f94c622db6fc2f78
Androoon <andrejsuslov308@gmail.com>
2024-06-16 13:45:47 +0300

    feat: added different password dtos

---
+++ auth/src/main/java/by/kladvirov/dto/EmailDto.java
@@ -0,0 +1,12 @@
+package by.kladvirov.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class EmailDto {
+
+    private String email;
+
+}
--- auth/src/main/java/by/kladvirov/dto/PasswordChangingDto.java
+++ auth/src/main/java/by/kladvirov/dto/PasswordChangingDto.java
@@ -1,8 +1,5 @@
 package by.kladvirov.dto;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,22 +11,10 @@ import lombok.NoArgsConstructor;
 @Builder
 public class PasswordChangingDto {
 
-    @NotEmpty(message = "Password can't be empty")
-    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$", message = "At least 8 characters, one uppercase letter, one lowercase " +
-            "letter and one number are required")
-    @Size(min = 8, max = 128, message = "Password has to be between 8 and 128 (this is too much, bro) characters")
     private String currentPassword;
 
-    @NotEmpty(message = "Password can't be empty")
-    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$", message = "At least 8 characters, one uppercase letter, one lowercase " +
-            "letter and one number are required")
-    @Size(min = 8, max = 128, message = "Password has to be between 8 and 128 (this is too much, bro) characters")
     private String newPassword;
 
-    @NotEmpty(message = "Password can't be empty")
-    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$", message = "At least 8 characters, one uppercase letter, one lowercase " +
-            "letter and one number are required")
-    @Size(min = 8, max = 128, message = "Password has to be between 8 and 128 (this is too much, bro) characters")
     private String confirmationPassword;
 
 }
